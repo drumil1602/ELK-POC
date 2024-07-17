8e9cb2505530619defc8990677377a8c82c893ba
drumil1602 <drumil.akhenia@pirimidtech.com>
2024-07-17 12:19:42 +0530

    initial setup

---
+++ src/test/java/com/poc/elk/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
